11cb03ed680c6bdb38b91fc5d5a210bdf35c04a1
fumiyasac <just1factory@gmail.com>
2023-10-07 06:16:26 +0900

    initial commit

---
+++ SutraCopyingShowCases/SutraCopyingShowCasesApp.swift
@@ -0,0 +1,20 @@
+//
+//  SutraCopyingShowCasesApp.swift
+//  SutraCopyingShowCases
+//
+//  Created by 酒井文也 on 2023/10/07.
+//
+
+import SwiftUI
+
+// TODO: MultiModule＆SwiftUIで構成する
+// TODO: 「The Composable Architecture」を利用した状態管理をする
+
+@main
+struct SutraCopyingShowCasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

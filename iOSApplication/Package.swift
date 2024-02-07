4abeb395ac804ceacd4d08855ab3a8de30ec44b9
fumiyasac <just1factory@gmail.com>
2024-02-08 06:57:39 +0900

    TCAを追加する

--- iOSApplication/Package.swift
+++ iOSApplication/Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         .library(name: "AppFeature", targets: ["AppFeature"])
     ],
     dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.7.3"),
     ],
     targets: [
         .target(name: "AppFeature")

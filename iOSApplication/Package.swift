92c4ef390b70de707005d366b6c29a9fb28ca8ce
fumiyasac <just1factory@gmail.com>
2024-02-09 23:39:08 +0900

    Common & ExtensionのModuleを追加する

--- iOSApplication/Package.swift
+++ iOSApplication/Package.swift
@@ -9,12 +9,20 @@ let package = Package(
     name: "iOSApplication",
     platforms: [.iOS(.v17)],
     products: [
-        .library(name: "AppFeature", targets: ["AppFeature"])
+        .library(name: "AppFeature", targets: ["AppFeature"]),
+        .library(name: "Common", targets: ["Common"]),
+        .library(name: "Extension", targets: ["Extension"]),
+//        .library(name: "Infrastructure", targets: ["Infrastructure"]),
+//        .library(name: "Repository", targets: ["Repository"])
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.7.3"),
     ],
     targets: [
-        .target(name: "AppFeature")
+        .target(name: "AppFeature"),
+        .target(name: "Common"),
+        .target(name: "Extension"),
+//        .target(name: "Infrastructure"),
+//        .target(name: "Repository")
     ]
 )

6c56d81c58e15ee247cc5992ed5c6ab0fd9a15bf
fumiyasac <just1factory@gmail.com>
2024-02-22 23:19:52 +0900

    Module名をリネームする

--- iOSApplication/Package.swift
+++ iOSApplication/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(name: "AppFeature", targets: ["AppFeature"]),
         .library(name: "Common", targets: ["Common"]),
-        .library(name: "Extension", targets: ["Extension"]),
+        .library(name: "Extensions", targets: ["Extensions"]),
         .library(name: "Infrastructure", targets: ["Infrastructure"]),
 //        .library(name: "Repository", targets: ["Repository"])
     ],
@@ -22,7 +22,7 @@ let package = Package(
     targets: [
         .target(name: "AppFeature"),
         .target(name: "Common"),
-        .target(name: "Extension"),
+        .target(name: "Extensions"),
         .target(name: "Infrastructure",
                 dependencies: [
                     .product(name: "Apollo", package: "apollo-ios"),

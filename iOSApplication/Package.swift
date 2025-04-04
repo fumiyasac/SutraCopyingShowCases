398c1b073ff3f0a8222a4b05a2b5a08b930e8000
fumiyasac <just1factory@gmail.com>
2025-04-04 18:50:52 +0900

    Buildできる所まで整える

--- iOSApplication/Package.swift
+++ iOSApplication/Package.swift
@@ -16,9 +16,9 @@ let package = Package(
 //        .library(name: "Domain", targets: ["Domain"])
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.9.2"),
-        .package(url: "https://github.com/apollographql/apollo-ios", from: "1.9.1"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage", from: "5.19.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.18.0"),
+        .package(url: "https://github.com/apollographql/apollo-ios", from: "1.19.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage", from: "5.21.0"),
     ],
     targets: [
         .target(name: "AppFeature"),
